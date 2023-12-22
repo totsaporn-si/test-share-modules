4b591a1abd47207e34541c6d0afb8acf528afdb2
Totsaporn Siripokkul <totsaporn.si@TDMB0170.local>
2023-12-22 14:51:48 +0700

    Initial source commit

---
+++ src/main/java/com/codelabs/test_share_modules/model/ShareModel1.kt
@@ -0,0 +1,5 @@
+package com.codelabs.test_share_modules.model
+
+data class ShareModel1(
+    val testField1: String
+)
---
+++ src/main/java/com/codelabs/test_share_modules/usecase/ShareUseCase.kt
@@ -0,0 +1,8 @@
+package com.codelabs.test_share_modules.usecase
+
+class ShareUseCase {
+
+    fun testX() {
+
+    }
+}
